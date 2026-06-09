ac8c25929c6ce066634cb49837443c9eb0be7281
Apoorva Shrivastava <apoorvashrivastava792@gmail.com>
2026-06-09 15:13:32 +0530

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -1,42 +1,7 @@
-// const nodemailer = require("nodemailer");
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_APP_PASSWORD,
-//   },
-// });
-
-// const msg = {
-//   subject: "Happy Birthday 🎂",
-//   body: "Wishing you a wonderful birthday ❤️,please check once if u get time",
-// };
-
-// async function sendEmail() {
-//   await transporter.sendMail({
-//     from: `Bunny 🐰 <${process.env.GMAIL_USER}>`, // ✅ fixed
-//     to: process.env.TO_EMAILS,
-//     subject: msg.subject,
-//     text: msg.body,
-//     html: `
-//       <h2>${msg.subject}</h2>
-//       <p>${msg.body}</p>
-//       <a href="${process.env.WEBSITE_URL}">Click Here 🎁</a>
-//     `,
-//   });
-
-//   console.log("✅ Email sent successfully");
-// }
-
-// sendEmail();
-
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
@@ -45,19 +10,19 @@ const transporter = nodemailer.createTransport({
 
 const msg = {
   subject: "Happy Birthday 🎂",
-  body: "Wishing you a wonderful birthday ❤️, please check once if u get time",
+  body: "Wishing you a wonderful birthday ❤️,please check once if u get time",
 };
 
 async function sendEmail() {
   await transporter.sendMail({
-    from: `Bunny 🐰 <${process.env.GMAIL_USER}>`,
+    from: `Bunny 🐰 <${process.env.GMAIL_USER}>`, // ✅ fixed
     to: process.env.TO_EMAILS,
     subject: msg.subject,
     text: msg.body,
     html: `
       <h2>${msg.subject}</h2>
       <p>${msg.body}</p>
-      process.env.WEBSITE_URL}">Click Here 🎁</a>
+      <a href="${process.env.WEBSITE_URL}">Click Here 🎁</a>
     `,
   });
 
@@ -65,3 +30,4 @@ async function sendEmail() {
 }
 
 sendEmail();
+
