8319b84fa8019d8b94706fb9ee6af599bcdc7b08
Apoorva Shrivastava <apoorvashrivastava792@gmail.com>
2026-07-24 02:46:33 +0530

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -1,32 +1,27 @@
-const nodemailer = require("nodemailer");
+onst nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
-  },
+service: "gmail",
+auth: {
+user: process.env.GMAIL_USER,
+pass: process.env.GMAIL_APP_PASSWORD,
+},
 });
 
 const msg = {
-  subject: "Happy Birthday 🎂",
-  body: "Wishing you a wonderful birthday ❤️ and all the love available in the world ",
-};
+subject: "Happy Birthday 🎂",
+body: "Wishing you a wonderful birthday ❤️,please check once if u get time,love form bunny that will always be there",};
 
 async function sendEmail() {
-  await transporter.sendMail({
-    from: `Bunny 🐰 <${process.env.GMAIL_USER}>`, // ✅ fixed
-    to: process.env.TO_EMAIL,
-    subject: msg.subject,
-    text: msg.body,
-    html: `
-      <h2>${msg.subject}</h2>
-      <p>${msg.body}</p>
-      ${process.env.WEBSITE_URL}Click Here 🎁</a>
-    `,
-  });
+await transporter.sendMail({
+from: Bunny 🐰 ❤️<${process.env.GMAIL_USER}>, // ✅ fixed
+to: process.env.TO_EMAIL,
+subject: msg.subject,
+text: msg.body,
+html: <h2>${msg.subject}</h2> <p>${msg.body}</p> <a href="${process.env.WEBSITE_URL}">Click Here 🎁</a> ,
+});
 
-  console.log("✅ Email sent successfully");
+console.log("✅ Email sent successfully");
 }
 
 sendEmail();
